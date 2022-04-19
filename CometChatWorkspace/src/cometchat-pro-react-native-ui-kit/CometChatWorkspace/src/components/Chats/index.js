5bf0148858a3ea58ffe563a2d8693e920ff6352e
SwethaSreeAvula <swetha.avula@cometchat.com>
2022-04-19 20:52:23 +0530

    bug fix

--- CometChatWorkspace/src/cometchat-pro-react-native-ui-kit/CometChatWorkspace/src/components/Chats/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CometChatConversationList } from './CometChatConversationList';
-export { default as CometChatConversationListItem } from './CometChatConversationListItem';
-export { default as CometChatConversationListWithMessages } from './CometChatConversationListWithMessages';
