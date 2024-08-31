36659690c09305823229b013b39621fb00610a1f
xuannhiminh <xuannhiminh>
2024-08-31 22:45:54 +0700

    add credencial for run the app

--- AppConstants.ts
+++ AppConstants.ts
@@ -1,5 +1,5 @@
 export const AppConstants = {
-    APP_ID: 'xxxxxxxxxxxxx', // Enter your App ID
-    REGION: 'xx', // Enter your App Region
-    AUTH_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Enter your App Auth Key
+    APP_ID: '2631422aa682a3da', // Enter your App ID
+    REGION: 'us', // Enter your App Region
+    AUTH_KEY: 'fecda6f16f0dd06e774f9d898da0041a7ee7eccc', // Enter your App Auth Key
 }
